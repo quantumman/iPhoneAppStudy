ab1b020af6f6ce13fa603676ce744aa86916715d
Shoadi Yokoyama <quantumcars@gmail.com>
2014-04-15 09:35:57 +0900

    Add CatAge project

---
+++ CatAge/CatAge/CatAge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CatAge
+//
+//  Created by quantumman on 2014/04/15.
+//  Copyright (c) 2014年 quantumman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CatAge/CatAge/CatAge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CatAge
+//
+//  Created by quantumman on 2014/04/15.
+//  Copyright (c) 2014年 quantumman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
